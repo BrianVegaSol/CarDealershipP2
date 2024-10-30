8e6a5aff9d80bdd625d7c7098ed595f3b2fa00a6
BrianVegaSol <brivegasol@gmail.com>
2024-10-30 15:50:29 -0700

    kermit

--- src/main/java/com/pluarlsight/Contract.java
+++ src/main/java/com/pluarlsight/Contract.java
@@ -1,4 +1,36 @@
 package com.pluarlsight;
 
-public class Contract {
+abstract public class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private String vehicleSold;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    public Contract (String date, String customerName, String customerEmail, String vehicleSold,
+                     double totalPrice, double monthlyPayment) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+        this.totalPrice = totalPrice;
+        this.monthlyPayment = monthlyPayment;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getVehicleSold() {
+        return vehicleSold;
+    }
 }
--- src/main/java/com/pluarlsight/Main.java
+++ src/main/java/com/pluarlsight/Main.java
@@ -1,7 +1,16 @@
 package com.pluarlsight;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DealershipFileManager.getDealership();
+        if (!UserInterface.exitApp) {
+            UserInterface.display();
+        }
+        UserInterface.scan.close();
+        //close bufferreader
     }
 }
