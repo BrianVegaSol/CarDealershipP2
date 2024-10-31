d8e206dacc9f37c3825dbeb690fdedb38b9da0fe
BrianVegaSol <brivegasol@gmail.com>
2024-10-31 11:04:41 -0700

    Skeleton of SalesContract class
    Signed-off-by: BrianVegaSol <brivegasol@gmail.com>

--- src/main/java/com/pluarlsight/SalesContract.java
+++ src/main/java/com/pluarlsight/SalesContract.java
@@ -1,5 +1,19 @@
 package com.pluarlsight;
 
-public class SalesContract {
+public class SalesContract extends Contract{
 
+    public SalesContract(String date, String customerName, String customerEmail, String vehicleSold, double totalPrice,
+                         double monthlyPayment) {
+        super(date, customerName, customerEmail, vehicleSold, totalPrice, monthlyPayment);
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        return 0;
+    }
 }
