edb4109f0a11aca3a9968024073ade865f2cd664
Daniela Martinez <daniela09986@gmail.com>
2024-10-31 13:21:35 -0700

    added leaseContract class and added  to userinterface (still in the works)
    Signed-off-by: BrianVegaSol <brivegasol@gmail.com>

--- src/main/java/com/pluarlsight/DealershipFileManager.java
+++ src/main/java/com/pluarlsight/DealershipFileManager.java
@@ -38,6 +38,7 @@ public class DealershipFileManager {
     }
 
     public static void saveDealership(Dealership dealership) {
+
     }
 
 }
---
+++ src/main/java/com/pluarlsight/LeaseContract.java
@@ -0,0 +1,29 @@
+package com.pluarlsight;
+
+public class LeaseContract extends Contract {
+    private double originalPrice;
+    private double leaseFeePercent = 0.07;
+    private double interestRate = 0.04;
+    private int leaseTerm = 36; //all leases financed at 4% for 36 mo.
+
+
+    public LeaseContract(String date, String customerName, String customerEmail, String vehicleSold, double originalPrice, double totalPrice, double monthlyPayment) {
+       // i still need to fix this -dm
+        super(date, customerName, customerEmail, vehicleSold, totalPrice,monthlyPayment );
+
+        this.originalPrice = originalPrice;
+
+    }
+
+    //@Override
+    public double getTotalPrice() {
+        return originalPrice * (1 + leaseFeePercent);
+    }
+   // @Override
+    public double getMonthlyPayment() {
+        double principal = getTotalPrice() * 0.5; //50% of og price for the lease
+        double monthlyRate = interestRate / 12;
+        return (principal * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -leaseTerm));
+    }
+
+}
