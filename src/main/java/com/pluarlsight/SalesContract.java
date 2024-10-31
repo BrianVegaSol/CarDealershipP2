22a563d58fc28b037dd673fd7b5d97467bd717c4
BrianVegaSol <brivegasol@gmail.com>
2024-10-31 11:41:22 -0700

    Stuck on how to pull Vehicle.getPrice()
    Signed-off-by: BrianVegaSol <brivegasol@gmail.com>

--- src/main/java/com/pluarlsight/SalesContract.java
+++ src/main/java/com/pluarlsight/SalesContract.java
@@ -1,6 +1,8 @@
 package com.pluarlsight;
 
-public class SalesContract extends Contract{
+import java.util.Scanner;
+
+public class SalesContract extends Contract {
 
     public SalesContract(String date, String customerName, String customerEmail, String vehicleSold, double totalPrice,
                          double monthlyPayment) {
@@ -9,11 +11,50 @@ public class SalesContract extends Contract{
 
     @Override
     public double getTotalPrice() {
-        return 0;
+        final double SALES_TAX_AMOUNT = super.totalPrice * .95;
+        final int RECORDING_FEE = 100;
+
+        double processingFee = 0;
+        //Processing Fee Calc
+        if (super.totalPrice > 10_000) {
+            processingFee = 295;
+        } else {
+            processingFee = 495;
+        }
+
+        double fees = SALES_TAX_AMOUNT + RECORDING_FEE + processingFee;
+
+
+
+        return totalPrice;
     }
 
     @Override
     public double getMonthlyPayment() {
+        boolean wantsFinance = false;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Do you want to finance your vehicle?\n1) Yes\n2) No");
+        int financeInput = scan.nextInt();
+        if (financeInput == 1) {
+            wantsFinance = true;
+        }
+        double loanRate;
+        int loanTermInMonths;
+        //Loan Term & Month Calc
+        if (wantsFinance) {
+        //Loan Calc
+        if (super.totalPrice > 10_000) {
+            loanRate = .0425;
+            loanTermInMonths = 48;
+        } else {
+            loanRate = .0525;
+            loanTermInMonths = 24;
+        }
+        //TODO might not display properly
+            System.out.printf("Your loan rate is %.2f%%\nYour Loan Term is %d months", loanRate, loanTermInMonths);
+        } else {
+            return 0;
+        }
         return 0;
     }
 }
