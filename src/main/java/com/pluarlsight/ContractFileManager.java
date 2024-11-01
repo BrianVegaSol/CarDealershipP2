f8dbad2068a99cea818d3b999266d396d42c6162
BryanG2442 <bryangarcia2442@gmail.com>
2024-11-01 13:13:47 -0700

    Test
    Signed-off-by: BrianVegaSol <brivegasol@gmail.com>

--- src/main/java/com/pluarlsight/ContractFileManager.java
+++ src/main/java/com/pluarlsight/ContractFileManager.java
@@ -2,6 +2,8 @@ package com.pluarlsight;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -54,20 +56,39 @@ public class ContractFileManager {
                             salesTaxAmount, recordingFee, processingFee, totalPrice, wantsToFinance, monthlyPayment);
                     SalesContract.salesList.add(sales);
                 }
-                /*if (Contract.contract instanceof SalesContract) {
-                    System.out.println();
-                }*/
             }
+
+
             //TODO cant remember what the Exception is but might wanna add anotha one
-        } catch (IOException |
-                 IndexOutOfBoundsException e) {
+        } catch (IOException | IndexOutOfBoundsException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
     }
 
-    public static void saveContract(Dealership dealership) {
 
+    public void writeContract(Contract contract) {
+        try {
+            FileWriter filewriter = new FileWriter("src/contracts.csv", true);
+            BufferedWriter buffWriter = new BufferedWriter(filewriter);
+            Dealership dealership = DealershipFileManager.getDealership();
+            Vehicle vehicle = null;
+            for (Vehicle vehicleLoop : Dealership.inventory) {
+                if (contract.vehicleSold.equalsIgnoreCase(Integer.toString(vehicleLoop.getVINNumber()))) {
+                    vehicle = vehicleLoop;
+                }
+            }
+            if (contract instanceof LeaseContract) {
+                buffWriter.write("LEASE|" + contract.getDate() + "|" + contract.getCustomerName()
+                        + "|" + contract.getCustomerEmail() + "|" + contract.getVehicleSold() + "|" + vehicle.getYear()
+                        + "|" + vehicle.getMake() + "|" + vehicle.getModel() + "|" + vehicle.getVehicleType() + "|" +
+                        vehicle.getColor() + "|" + vehicle.getOdometer() + "|" + vehicle.getPrice() + "|" + contract
+                );
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing file has occurred");
+        }
     }
-}
 
-
+    public static void saveContract(Dealership dealership) {
+        }
+    }
--- src/main/java/com/pluarlsight/Main.java
+++ src/main/java/com/pluarlsight/Main.java
@@ -6,17 +6,11 @@ import java.util.Scanner;
 //aaaa
 public class Main {
     public static void main(String[] args) {
-        /*SalesContract s = new SalesContract("1","Bob","@me","Ford Explorer",
-                1_000,50);*/
-        //System.out.println(s.getMonthlyPayment());
         DealershipFileManager.getDealership();
         if (!UserInterface.exitApp) {
             UserInterface.display();
         }
         UserInterface.scan.close();
         //close bufferreader
-
-        //code written by Brian for example
-        //New text
     }
 }
