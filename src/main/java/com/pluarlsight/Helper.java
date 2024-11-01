13b577fe31a85d5547ddd0da9d5506b533081aec
BrianVegaSol <brivegasol@gmail.com>
2024-11-01 14:33:08 -0700

    showCurrentTime()
    Signed-off-by: BrianVegaSol <brivegasol@gmail.com>

--- src/main/java/com/pluarlsight/Helper.java
+++ src/main/java/com/pluarlsight/Helper.java
@@ -1,4 +1,13 @@
 package com.pluarlsight;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Helper {
+    public static void showCurrentTime() {
+        LocalTime currentTime = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String formattedTime = currentTime.format(formatter);
+        System.out.println("Current Time: " + formattedTime);
+    }
 }
