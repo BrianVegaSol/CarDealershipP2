9f1a421d7920dc0fafc24a84be6cde81a8d03928
Daniela Martinez <daniela09986@gmail.com>
2024-11-01 14:34:49 -0700

    Just created a helper class for date and time!!!!!

---
+++ src/main/java/com/pluarlsight/Helper.java
@@ -0,0 +1,11 @@
+package com.pluarlsight;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+public class Helper {
+    public static void showCurrentTime() {
+        LocalTime currentTime = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String formattedTime = currentTime.format(formatter);
+        System.out.println("Current Time: " + formattedTime); }
+}
